370516a584450b4fa01def9985865f468df16df6
francisca eze <phillipfrancisca@gmail.com>
2021-06-04 17:06:56 +0100

    add code for responsiveness

--- src/components/FormBody.js
+++ src/components/FormBody.js
@@ -3,53 +3,53 @@ import Styled from "styled-components";
 import { Form, Button } from "antd";
 import { ArrowRightOutlined } from "@ant-design/icons";
 
-const FormBody = ({ children, title, description }) => {
-  const [form] = Form.useForm();
-  const [, forceUpdate] = useState({});
+const FormBody = ({ children, title, description, next }) => {
+    const [form] = Form.useForm();
+    const [, forceUpdate] = useState({});
 
-  // To disable submit button at the beginning.
-  useEffect(() => {
-    forceUpdate({});
-  }, []);
+    // To disable submit button at the beginning.
+    useEffect(() => {
+        forceUpdate({});
+    }, []);
 
-  const onFinish = (values: any) => {
-    console.log("Finish:", values);
-  };
+    const onFinish = (values) => {
+        console.log("Finish:", values);
+    };
 
-  return (
-    <StyledDetails>
-        <div>
-            <h1>{title}</h1>
-            <p className="form-description">{description}</p>
+    return (
+        <StyledDetails>
+            <div>
+                <h1>{title}</h1>
+                <p className="form-description">{description}</p>
 
-        </div>
+            </div>
 
-      <Form form={form} layout="inline" onFinish={onFinish}>
-        {children}
+            <Form form={form} layout="inline" onFinish={onFinish}>
+                {children}
 
-        <Form.Item shouldUpdate>
-          {() => (
-            <Button
-              type="primary"
-              htmlType="submit"
-              // disabled={
-              //     !form.isFieldsTouched(true) ||
-              //     !!form.getFieldsError().filter(({ errors }) => errors.length).length
-              // }
-            >
-              Proceed to next Step <ArrowRightOutlined />
-            </Button>
-          )}
-        </Form.Item>
-      </Form>
-    </StyledDetails>
-  );
+                <Form.Item shouldUpdate>
+                    {() => (
+                        <Button
+                            type="primary"
+                            onClick={next}
+                        //   htmlType="submit"
+                        // disabled={
+                        //     !form.isFieldsTouched(true) ||
+                        //     !!form.getFieldsError().filter(({ errors }) => errors.length).length
+                        // }
+                        >
+                            Proceed to next Step <ArrowRightOutlined />
+                        </Button>
+                    )}
+                </Form.Item>
+            </Form>
+        </StyledDetails>
+    );
 };
 
 const StyledDetails = Styled.div`
     box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px;
     max-width: 70%;
-    /* width: 60%; */
     h1{
         color: rgba(0, 0, 0, 0.85);
         text-align: center;
@@ -57,19 +57,9 @@ const StyledDetails = Styled.div`
         font-weight: 600;
     }
     Form{
-        padding: 20px;
+        padding: 20px 20px 0;
     }
-    /* .ant-btn-primary[disabled], .ant-btn-primary[disabled]:hover, .ant-btn-primary[disabled]:focus, .ant-btn-primary[disabled]:active {
-    background: #2657ff;
-    color: white;
-    width: 100%;
-    font-size: 1.6rem;
-    margin: 4.5rem 3rem;
-    padding: 12px 0;
-    border-radius: 10px;
-    height: 100%;
-    width: 70%;
-    } */
+    
     .form-description{
         width: 50%;
         margin: 0 auto;
@@ -95,6 +85,29 @@ const StyledDetails = Styled.div`
     .ant-form-inline .ant-form-item {
         width: 100%;
     }
+    @media only screen and (max-width:1026px){
+     max-width: 85%;
+   
+
+ }
+ @media only screen and (max-width:600px){
+     max-width: 100%;
+    .ant-btn-primary, .ant-btn-primary:hover, .ant-btn-primary:focus, .ant-btn-primary:active {
+        width: 100%;
+    }
+
+ }
+
+ @media only screen and (max-width:600px){
+    h1{
+        width: 80%;
+        margin: 0 auto;
+
+    }
+
+ }
+
+
 
 `;
 export default FormBody;
