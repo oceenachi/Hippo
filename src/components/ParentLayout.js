fe37253d067b652c9e051b73713c08162ff61d0f
francisca eze <phillipfrancisca@gmail.com>
2021-06-04 17:07:30 +0100

    add code for responsiveness

--- src/components/ParentLayout.js
+++ src/components/ParentLayout.js
@@ -6,12 +6,16 @@ const ParentLayout = () => {
     return (
         <Wrapper className="wrapper">
             <nav className="parent-navbar">
-                NAV BAR
+                <div className="nav-img">
+                    <img src="./images/payhippo.png" alt="logo"/>
+                </div>
             </nav>
                 <ProgressStep className="mid-section"/>
+
+
             
             <footer>
-                footer text
+                <p className="footer-text">© 2021 payhippo.ng | Re-Engineering Digital SME Lending | All rights reserved.</p>
             </footer>
 
         </Wrapper>
@@ -21,22 +25,55 @@ const ParentLayout = () => {
 
 const Wrapper = Styled.div`
 width: 100vw;
-height: 100vh;
+min-height: 100vh;
 display: flex;
 flex-direction: column;
 
+
 .parent-navbar{
     width: 100%;
-    height: 70px;
-    background: green;
+    height: 90px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 }
-
+.nav-img{
+    width: 10%;
+    margin: 0 auto;
+}
 .mid-section{
     border: 2px solid red;
 }
 footer{
-    background: yellow;
     height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    p{
+        text-align: center;
+        width: 60%;
+        
+    }
+}
+@media only screen and (max-width:770px){
+ 
+ .nav-img {
+    width: 20%;
+    margin: 0 auto;
+    }
+
+    .footer-text{
+        margin-top: 5rem;
+    }
+}
+@media only screen and (max-width:420px){
+ 
+ .nav-img {
+    width: 30%;
+    margin: 0 auto;
+    }
+
 }
 
 `;
