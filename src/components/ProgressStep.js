82be57e31c07c1c69a699ad21131e85c6f038d70
francisca eze <phillipfrancisca@gmail.com>
2021-06-04 14:48:02 +0100

    add progess steps

--- src/components/ProgressStep.js
+++ src/components/ProgressStep.js
@@ -1,49 +1,53 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Steps, Popover } from 'antd';
+import { Steps } from 'antd';
 import Styled from "styled-components";
-import PersonalDetailsForm from "./PersonalDetailsForm"
+import FormBody from "./FormBody"
+import VerificationForm from './VerificationForm';
+import BusinessDetailsForm from './BusinessDetailsForm';
+import AccountDetails from './AccountDetails';
+import PersonalDetails from "./PersonalDetails";
 
 
 
 const { Step } = Steps;
 
-const CustomDot = (dot, { status, index }) => (
-  <Popover
-    content={
-      <span>
-        step {index} status: {status}
-      </span>
-    }
+// const CustomDot = (dot, { status, index }) => (
+//   <Popover
+//     content={
+//       <span>
+//         step {index} status: {status}
+//       </span>
+//     }
 
-  >
-    {dot}
-  </Popover>
-);
+//   >
+//     {dot}
+//   </Popover>
+// );
 
 
 
 const steps = [
   {
     title: 'Personal Details',
-    content: <PersonalDetailsForm />,
+    content: <FormBody description="" title="Tell us about you"><PersonalDetails/></FormBody>,
     description: "Tell us about you"
   },
   {
     title: 'Verification',
-    content: "other",
+    content: <FormBody description="We have sent and SMS message with your verification code to your phone number" title="Verify Your Phone Number"><VerificationForm/></FormBody>,
     description: "SMS message with your verification code to your phone number"
 
   },
   {
     title: 'Business Details',
-    content: 'Tell us about your business',
+    content: <FormBody description="Bussiness account Details" title="Tell us about your Bank account details"><BusinessDetailsForm/></FormBody>,
     description: "Tell us about your business"
 
   },
   {
     title: 'Bank/Business Account Details',
-    content: 'Last-content',
+    content: <FormBody description="Business Account Details" title="Tell us about your Bank Account"><AccountDetails/></FormBody>,
     description: "Enter your bank account details"
 
   },
@@ -59,18 +63,18 @@ function ProgressStep() {
     setCurrent(current);
   };
 
-  const next = () => {
-    setCurrent(current + 1);
-  };
+  // const next = () => {
+  //   setCurrent(current + 1);
+  // };
 
-  const prev = () => {
-    setCurrent(current - 1);
-  };
+  // const prev = () => {
+  //   setCurrent(current - 1);
+  // };
 
   return (
     <StyledProgressSteps>
       <Steps current={current} className="steps-parent" onChange={onChange} >
-        {steps.map((item, index) => (
+        {steps.map((item) => (
           <>
             <Step key={item.title} title={item.title} description={item.description} className="progress-steps" />
           </>
@@ -87,8 +91,10 @@ function ProgressStep() {
 
 const StyledProgressSteps = Styled.div`
     width: 80%;
-    height: 100%;
     margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
 
 
 .ant-steps-item-process > .ant-steps-item-container > .ant-steps-item-icon {
@@ -98,7 +104,6 @@ const StyledProgressSteps = Styled.div`
   color: #2657ff;
 
 }
-
 .ant-steps-item-finish .ant-steps-item-icon {
     border-color: #2657ff;
 }
