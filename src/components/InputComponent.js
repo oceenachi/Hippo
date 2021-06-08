24f6f74de7917ed99060c058edafffc5f14a90ea
francisca eze <phillipfrancisca@gmail.com>
2021-06-08 23:58:29 +0100

    allow validation for input component

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -4,13 +4,14 @@ import { Form, Input } from "antd";
 import * as AntdIcons from '@ant-design/icons';
 
 
-export const FormInputComponent = ({ details }) => {
+export const FormInputComponent = ({ details, data, updateData, form,  error, setError }) => {
 
 
+    
     const { name, label, rules, icon, placeholder , type} = details;
 
     const prefixSelector = (
-        <Form.Item name="prefix" noStyle>
+        <Form.Item name="prefix" noStyle >
           <div
             style={{
               width: 60,
@@ -24,16 +25,29 @@ export const FormInputComponent = ({ details }) => {
 
     let AntdIcon = AntdIcons[icon] ;
 
+    function onChange(e) {
+        setError({ ...error, [name]: ""});
+        updateData({ ...data, [name]: e.target.value});
+    }
+
     return (
-        <StyledInput>
+        <StyledInput error={error && error[name]}>
             <Form.Item
                 className="form-input-item"
                 name={name}
                 label={label}
                 rules={rules}
+
             >
-                <Input prefix={<AntdIcon className="site-form-item-icon" />} placeholder={placeholder} addonBefore={type && type === "num" ? prefixSelector : ""}/>
+            
+                <Input prefix={<AntdIcon className="site-form-item-icon" />} 
+                    placeholder={placeholder} addonBefore={type && type === "num" ? prefixSelector : ""} 
+                    onChange={onChange}
+                    
+                />
+
             </Form.Item>
+            { error && error[name] && <p color="red" className="error-alert form-item-explain-error ant-form-item-explain">{error[name]}</p>}
 
         </StyledInput>
 
@@ -66,6 +80,24 @@ const StyledInput = styled.div`
 }
 .ant-form-item-label > label {
     margin-top: 1rem;
+}
+
+.ant-select:not(.ant-select-disabled):hover .ant-select-selector {
+    border-color: ${({error}) => `${error? '#ff4d4f':  '#40a9ff'} !important`};
+    border-right-width: 1px !important;
+}
+.form-item-explain-error {
+    color: #ff4d4f;
+}
+
+.ant-form-item-explain {
+    clear: both;
+    min-height: 24px;
+    font-size: 14px;
+    line-height: 1.5715;
+}
+.error-alert.form-item-explain-error.ant-form-item-explain{
+    margin-bottom: 0;
 }
 
 @media only screen and  (max-width: 770px){
